9c5b391dbb15a4bcbff65c270cced11b6d249a5c
Binnur KURT <deepcloudlabs@gmail.com>
2024-11-07 10:14:59 +0300

    initial commit

---
+++ monitoring-exercises/src/com/example/CopyFile.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
+public class CopyFile {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		try (FileChannel srcChannel = new FileInputStream(new File("src/data", "infile.iso")).getChannel();
+				FileChannel dstChannel = new FileOutputStream(new File("src/data", "outfile.iso")).getChannel();) {
+			dstChannel.transferFrom(srcChannel, 0, srcChannel.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
